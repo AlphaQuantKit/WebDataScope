49b165a1ab97032476b70108aced222b9747caa8
Dear <104989650+Worldfickler@users.noreply.github.com>
2026-09-18 14:08:59 +0800

    Update login URL in loginAndOpenWqManager function
    更新wqmanager地址

--- src/background/background.js
+++ src/background/background.js
@@ -529,7 +529,7 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 async function loginAndOpenWqManager(wqId, currentTabId) {
     // 在当前标签页打开登录页面
     currentTabId = await new Promise((resolve, reject) => {
-        chrome.tabs.create({ url: 'https://wqmanager.qzz.io/login', active: true }, (tab) => {
+        chrome.tabs.create({ url: 'https://wqmanager.icu/login', active: true }, (tab) => {
             if (chrome.runtime.lastError) return reject(chrome.runtime.lastError);
             resolve(tab.id);
         });
